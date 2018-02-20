3ae55172fcd1f61c1178689dd7ef3cc70677f220
petersaints <petersaints@gmail.com>
2018-02-20 12:26:32 +0000

    - Cleaning unused code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import _ from 'lodash';
-import { Coordinator } from '@yanux/coordinator'
-
-function component() {
-    var element = document.createElement('div');
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-}
- 
+import { Coordinator } from '@yanux/coordinator' 
 document.body.appendChild(component());
 
 var coordinator = new Coordinator("ws://localhost:6020/deepstream", { username: "john", password: "doe" });

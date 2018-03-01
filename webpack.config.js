9f759c963d15830cf9dcfc3de98a1283af37c55b
petersaints <petersaints@gmail.com>
2018-03-01 19:38:49 +0000

    Package Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  devtool: "source-map",
+  mode: "development",
   entry: './src/index.js',
+  devtool: "source-map",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')

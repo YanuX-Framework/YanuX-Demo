60794270201f396407a8ce19063ea0c295e4c4de
petersaints <petersaints@gmail.com>
2018-08-28 15:28:48 +0100

    - Added yx-component support!

--- src/index.js
+++ src/index.js
@@ -5,16 +5,33 @@ import * as queryString from 'query-string';
 const params = queryString.parse(location.hash);
 let url = "http://localhost:3030";
 
+function yxDisplay(displayClasses, hiddenClasses) {
+    if (hiddenClasses) {
+        hiddenClasses.split(',').forEach(c => $("." + c.trim()).css("display", "none"));
+    }
+    if (displayClasses) {
+        displayClasses.split(',').forEach(c => $("." + c.trim()).css("display", "block"));
+    }
+    $(".yx-always-visible").css("display", "block");
+}
+yxDisplay(params.displayClasses || null, params.hiddenClasses || "yx-view, yx-control");
 /** ------------------------------------------------------------------------ **/
 /** -------------------------- YanuX Coordinator --------------------------- **/
 /** ------------------------------------------------------------------------ **/
 let app = new App(params.app || "demo");
-let user = new User(params.username || "pedro@albuquerques.net", params.password || "topsecret");
-let coordinator = new FeathersCoordinator(params.url || url, app, user);
+let user = new User(params.username || "test_user_0@yanux.org", params.password || "topsecret");
+$("#welcome").append(user.username);
 
+let coordinator = new FeathersCoordinator(params.url || url, app, user);
 function setSquareColor(color) {
     if (color) {
         $(".square").css("background-color", color)
+    }
+}
+
+function setCurrentColorText(color) {
+    if (color) {
+        $("#current-color").text(color);
     }
 }
 
@@ -22,50 +39,59 @@ coordinator.init().then(data => {
     console.log('State:', data);
     if (data) {
         setSquareColor(data["squareColor"]);
+        setCurrentColorText(data["squareColor"]);
     }
 });
 
 coordinator.subscribe(data => {
     console.log('Data Changed: ', data);
     setSquareColor(data["squareColor"]);
+    setCurrentColorText(data["squareColor"]);
 })
 
 $(".red-button").click(function (evt) {
     console.log("Clicked Red Button:", evt);
-    coordinator.setData({ squareColor: "red" });
+    coordinator.setData({ squareColor: "#ff0000" });
+});
+
+$(".green-button").click(function (evt) {
+    console.log("Clicked Green Button:", evt);
+    coordinator.setData({ squareColor: "#00ff00" });
 });
 
 $(".blue-button").click(function (evt) {
     console.log("Clicked Blue Button:", evt);
-    coordinator.setData({ squareColor: "blue" });
+    coordinator.setData({ squareColor: "#0000ff" });
 });
+
 /** ------------------------------------------------------------------------ **/
 /** -------------------------- Web Bluetooth API --------------------------- **/
 /** ------------------------------------------------------------------------ **/
-function webBluetoothApi() {
+ /** NOTE: This could actually work and possibly replace the need of
+ * platform specific code once the following conditions are met:
+ * - watchAdvertisements() is actually implemented in at least one
+ * browser for each of the most common end-user platforms (e.g.,
+ * Google Chrome on Windows, Linux, macOS, Android and iOS)
+ * - It becomes possible to scan for nearby advertisements without
+ * user intervention. I understand that there are privacy concerns,
+ * but asking for permission once the page is loaded should be more
+ * than enough.
+ * - The standard should mature and become more than just a draft.
+ * So far the only browser vendor interested in the Web Bluetooth API
+ * seems to be Google, yet Chrome doesn't have it enabled across all
+ * platforms. Some are unsupported (e.g., Windows) and others are
+ * experimental and hidden behind a flag (e.g., Linux) Mozilla and 
+ * Microsoft have not implemented it yet and they don't seem to be very
+ * interested in doing so. Opera, since it relies on Chromium/Blink, has
+ * it working as well. I supposse that other Chromium-based browsers can
+ * also just enable or provide a flag if they want it to.
+ **/
+/* function webBluetoothApi() {
     navigator.bluetooth.requestDevice({
         "acceptAllDevices": true
     }).then(device => {
         console.log('Device Found:', device);
-        /** NOTE: This could actually work and possibly replace the need of
-         * platform specific code once the following conditions are met:
-         * - watchAdvertisements() is actually implemented in at least one
-         * browser for each of the most common end-user platforms (e.g.,
-         * Google Chrome on Windows, Linux, macOS, Android and iOS)
-         * - It becomes possible to scan for nearby advertisements without
-         * user intervention. I understand that there are privacy concerns,
-         * but asking for permission once the page is loaded should be more
-         * than enough.
-         * - The standard should mature and become more than just a draft.
-         * So far the only browser vendor interested in the Web Bluetooth API
-         * seems to be Google, yet Chrome doesn't have it enabled across all
-         * platforms. Some are unsupported (e.g., Windows) and others are
-         * experimental and hidden behind a flag (e.g., Linux) Mozilla and 
-         * Microsoft have not implemented it yet and they don't seem to be very
-         * interested in doing so. Opera, since it relies on Chromium/Blink, has
-         * it working as well. I supposse that other Chromium-based browsers can
-         * also just enable or provide a flag if they want it to.
-         **/
+       
         device.watchAdvertisements();
         device.addEventListener('advertisementreceived', interpretIBeacon);
     });
@@ -88,4 +114,4 @@ function webBluetoothApi() {
             pathLossVs1m: txPowerAt1m - rssi
         });
     };
-};
+}; */

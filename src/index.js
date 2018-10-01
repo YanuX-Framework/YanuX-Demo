1c734dcc1a4716a1d4aecc969e6fc4fcf85b30ef
petersaints <petersaints@gmail.com>
2018-10-01 20:08:51 +0100

    Better Auth Integration using OAuth 2.0 Implicit Flow. Better flows can be implemented if need be (see YanuX Auth's comments about that matter)

--- src/index.js
+++ src/index.js
@@ -1,124 +1,100 @@
-import { Client, FeathersCoordinator, Credentials } from '@yanux/coordinator';
-import $ from 'jquery';
-import * as queryString from 'query-string';
+import { FeathersCoordinator, Credentials } from "@yanux/coordinator";
+import $ from "jquery";
+import * as queryString from "query-string";
 
-const params = queryString.parse(location.hash);
-
-function yxDisplay(displayClasses, hiddenClasses) {
-    if (hiddenClasses) {
-        hiddenClasses.split(',').forEach(c => $("." + c.trim()).css("display", "none"));
-    }
-    if (displayClasses) {
-        displayClasses.split(',').forEach(c => $("." + c.trim()).css("display", "block"));
-    }
-    $(".yx-always-visible").css("display", "block");
+function initUi(params) {
+    yxDisplay(params.displayClasses || null, params.hiddenClasses || null /* "yx-view, yx-control" */);
+    $("#login a").text("Login");
+    $("#login a").attr(
+        "href",
+        "http://localhost:3001/oauth2/authorize?client_id=yanux-demo-app&response_type=token&redirect_uri=" + window.location.href
+    );
 }
-yxDisplay(params.displayClasses || null, params.hiddenClasses || "yx-view, yx-control");
-/** ------------------------------------------------------------------------ **/
-/** -------------------------- YanuX Coordinator --------------------------- **/
-/** ------------------------------------------------------------------------ **/
-//const credentials = new Credentials("local", [params.username || "test_user_0@yanux.org", params.password || "topsecret"]);
-const credentials = new Credentials("yanux", [
-    "tZ0RxsvLgAe6KGhMAZIV8wP0VZRJSKzY9OFBcErLSHlIUdIuNyMFvqIZDuCKQXidqgTYc7GZMMSHX8HZBnF6a6sLMDtNdnfGyDGErrKX0w9u0i2Qc0xtYzfVLzBKezlpoxUqKaaY1hQWrdfcDyiq96sd0qyP9uRZlNyKtx8jMSWuAOcZKbi9cZYLjdeBcS7gyvS0NSRC56HodQYTQglc2yWLNqjHPDs6or2tjrNCRjb7ofyBZeVVgk031mPyr7uI",
-    "yanux-ips-desktop-client"
-]);
-$("#welcome").append(credentials[0]);
-
-const coordinator = new FeathersCoordinator(
-    params.url || "http://localhost:3002",
-    credentials,
-    params.app || "yanux-demo"
-);
 
 function setSquareColor(color) {
     if (color) {
         $(".square").css("background-color", color)
     }
 }
-
 function setCurrentColorText(color) {
     if (color) {
         $("#current-color").text(color);
     }
 }
-
-coordinator.init().then(data => {
-    console.log('State:', data);
-    if (data) {
-        setSquareColor(data["squareColor"]);
-        setCurrentColorText(data["squareColor"]);
+function yxDisplay(displayClasses, hiddenClasses) {
+    if (hiddenClasses) {
+        hiddenClasses.split(",").forEach(c => $("." + c.trim()).css("display", "none"));
     }
-});
+    if (displayClasses) {
+        displayClasses.split(",").forEach(c => $("." + c.trim()).css("display", "block"));
+    }
+    $(".yx-always-visible").css("display", "block");
+}
 
-coordinator.subscribe(data => {
-    console.log('Data Changed: ', data);
-    setSquareColor(data["squareColor"]);
-    setCurrentColorText(data["squareColor"]);
-})
+function initCoordinator(coordinator) {
+    coordinator.init().then(data => {
+        coordinator.subscribe(data => {
+            console.log("Data Changed:", data);
+            setSquareColor(data["squareColor"]);
+            setCurrentColorText(data["squareColor"]);
+        })
 
-$(".red-button").click(function (evt) {
-    console.log("Clicked Red Button:", evt);
-    coordinator.setData({ squareColor: "#ff0000" });
-});
-
-$(".green-button").click(function (evt) {
-    console.log("Clicked Green Button:", evt);
-    coordinator.setData({ squareColor: "#00ff00" });
-});
-
-$(".blue-button").click(function (evt) {
-    console.log("Clicked Blue Button:", evt);
-    coordinator.setData({ squareColor: "#0000ff" });
-});
-
-/** ------------------------------------------------------------------------ **/
-/** -------------------------- Web Bluetooth API --------------------------- **/
-/** ------------------------------------------------------------------------ **/
- /** NOTE: This could actually work and possibly replace the need of
- * platform specific code once the following conditions are met:
- * - watchAdvertisements() is actually implemented in at least one
- * browser for each of the most common end-user platforms (e.g.,
- * Google Chrome on Windows, Linux, macOS, Android and iOS)
- * - It becomes possible to scan for nearby advertisements without
- * user intervention. I understand that there are privacy concerns,
- * but asking for permission once the page is loaded should be more
- * than enough.
- * - The standard should mature and become more than just a draft.
- * So far the only browser vendor interested in the Web Bluetooth API
- * seems to be Google, yet Chrome doesn't have it enabled across all
- * platforms. Some are unsupported (e.g., Windows) and others are
- * experimental and hidden behind a flag (e.g., Linux) Mozilla and 
- * Microsoft have not implemented it yet and they don't seem to be very
- * interested in doing so. Opera, since it relies on Chromium/Blink, has
- * it working as well. I supposse that other Chromium-based browsers can
- * also just enable or provide a flag if they want it to.
- **/
-/* function webBluetoothApi() {
-    navigator.bluetooth.requestDevice({
-        "acceptAllDevices": true
-    }).then(device => {
-        console.log('Device Found:', device);
-       
-        device.watchAdvertisements();
-        device.addEventListener('advertisementreceived', interpretIBeacon);
-    });
-    function interpretIBeacon(event) {
-        var rssi = event.rssi;
-        var ibeaconData = event.manufacturerData.get(0x004C);
-        if (ibeaconData.byteLength != 23 ||
-            ibeaconData.getUint16(0, false) !== 0x0215) {
-            console.log({ isBeacon: false });
-        }
-        var uuidArray = new Uint8Array(ibeaconData.buffer, 2, 16);
-        var major = ibeaconData.getUint16(18, false);
-        var minor = ibeaconData.getUint16(20, false);
-        var txPowerAt1m = -ibeaconData.getInt8(22);
-        console.log({
-            isBeacon: true,
-            uuidArray,
-            major,
-            minor,
-            pathLossVs1m: txPowerAt1m - rssi
+        $(".red-button").click(function (evt) {
+            console.log("Clicked Red Button:", evt);
+            coordinator.setData({ squareColor: "#ff0000" });
         });
-    };
-}; */
+
+        $(".green-button").click(function (evt) {
+            console.log("Clicked Green Button:", evt);
+            coordinator.setData({ squareColor: "#00ff00" });
+        });
+
+        $(".blue-button").click(function (evt) {
+            console.log("Clicked Blue Button:", evt);
+            coordinator.setData({ squareColor: "#0000ff" });
+        });
+
+        if (data) {
+            console.log("State:", data);
+            console.log("User:", coordinator.user)
+            $("#welcome").append(coordinator.user.email);
+            $("#login a").text("Logout");
+            $("#login a").click(() => {
+                coordinator.logout();
+                $("#login a").text("Login");
+            })
+            setSquareColor(data["squareColor"]);
+            setCurrentColorText(data["squareColor"]);
+        }
+    }).catch(e => {
+        console.log(e);
+        alert('Try to log back in!')
+        coordinator.logout();
+        initUi(params);
+    });
+}
+
+/** ------------------------------------------------------------------------ **/
+/** -------------------------- YanuX Coordinator --------------------------- **/
+/** ------------------------------------------------------------------------ **/
+function main() {
+    const params = queryString.parse(location.hash);
+    console.log("Params:", params);
+    const coordinator = new FeathersCoordinator(
+        params.url || "http://localhost:3002",
+    );
+    //window.coordinator = coordinator;
+    if (coordinator.credentials) {
+        initCoordinator(coordinator);
+    } else if (params.access_token) {
+        coordinator.credentials = new Credentials("yanux", [
+            params.access_token,
+            params.app || "yanux-demo-app"
+        ]);
+        initCoordinator(coordinator);
+    } else {
+        initUi(params);
+    }
+}
+
+main();

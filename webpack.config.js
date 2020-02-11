868eab0ba8af31a4a87e5fe6145055a3f348b9b7
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-02-11 20:33:06 +0000

    - Package Updates
    - Replaced double quotes by single quotes on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: './src/index.js',
-  devtool: "source-map",
+  devtool: 'source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ]
   },
   node: {
